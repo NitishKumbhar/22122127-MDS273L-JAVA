de4ec6a82d768f7c3a7def6ecd14d088a8d24948
NitishKumbhar <kumbhar.nitish@msds.christuniversity.in>
2023-02-22 12:17:57 +0530

    lab1
    LAB1

---
+++ LAB-1/LAB.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LAB {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String st=s.nextLine();
+        System.out.println("Enter your age :");
+        int age=s.nextInt();
+        System.out.println("Enter your Class:");
+        String cs=s.nextLine();
+        System.out.println("Name :"+st);
+        System.out.println("Age :"+age);
+        System.out.println("Class :"+cs);
+  
+    }
+    
+}
